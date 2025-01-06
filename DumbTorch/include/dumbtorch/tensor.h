2b64b12540b0bfcca3fe6be231400ab1053ea2d4
niv <ni.h1@hotmail.com>
2025-01-06 20:44:10 -0300

    add tensors

--- DumbTorch/include/dumbtorch/tensor.h
+++ DumbTorch/include/dumbtorch/tensor.h
@@ -1,27 +1,79 @@
 #pragma once
+
 #include <vector>
 #include <memory>
+#include <string>
 
 namespace dumbtorch {
 
-    class Tensor;
-    class Function;
+    // Forward declarations
+    class AutogradContext;
+
+    enum class DeviceType {
+        CPU,
+        CUDA
+    };
+
+    enum class DType {
+        Float32,
+        Float64,
+        Int32,
+        Int64
+    };
 
     class Tensor {
     public:
-        
-        Tensor(const std::vector<float>& data,
-            const std::vector<int>& shape,
-            bool requires_grad = false);
+        // Constructors
+        Tensor() = default;  // Default constructor for empty tensor
 
-        std::vector<float> data_;
-        std::vector<int> shape_;
+        // Create tensor from raw data
+        template<typename T>
+        Tensor(const std::vector<T>& data, const std::vector<int64_t>& shape,
+            DType dtype = DType::Float32, DeviceType device = DeviceType::CPU);
 
-        bool requires_grad_;
-        std::vector<float> grad_;
-        std::shared_ptr<Function> grad_fn_; // function that created this Tensor
+        // Create tensor with given shape, initialized to zeros
+        Tensor(const std::vector<int64_t>& shape, DType dtype = DType::Float32,
+            DeviceType device = DeviceType::CPU);
 
-        void backward();
+        // Copy constructor and assignment operator (Rule of Five)
+        Tensor(const Tensor& other);
+        Tensor& operator=(const Tensor& other);
+        Tensor(Tensor&& other) noexcept;
+        Tensor& operator=(Tensor&& other) noexcept;
+        ~Tensor();
+
+        // Basic operations
+        Tensor operator+(const Tensor& other) const;
+        Tensor operator-(const Tensor& other) const;
+        Tensor operator*(const Tensor& other) const;
+        Tensor operator/(const Tensor& other) const;
+
+        // Utility methods
+        std::string toString() const;
+        std::vector<int64_t> getShape() const { return shape_; }
+        DType getDType() const { return dtype_; }
+        DeviceType getDevice() const { return device_; }
+        bool requiresGrad() const { return requires_grad_; }
+        void setRequiresGrad(bool requires_grad);
+
+        // Data access
+        template<typename T>
+        T* getData() const;
+
+    private:
+        // Internal data representation
+        std::shared_ptr<void> data_;
+        std::vector<int64_t> shape_;
+        DType dtype_ = DType::Float32;
+        DeviceType device_ = DeviceType::CPU;
+
+        // Autograd-related members
+        bool requires_grad_ = false;
+        std::shared_ptr<AutogradContext> grad_ctx_;
+
+        // Helper methods
+        int64_t calculateSize() const;
+        void allocateMemory();
     };
 
-}
+} // namespace dumbtorch
--- DumbTorch/src/tensor.cpp
+++ DumbTorch/src/tensor.cpp
@@ -1,1 +1,147 @@
 #include "dumbtorch/tensor.h"
+#include <stdexcept>
+#include <sstream>
+#include <cstring>
+
+namespace dumbtorch {
+
+    template<typename T>
+    Tensor::Tensor(const std::vector<T>& data, const std::vector<int64_t>& shape,
+        DType dtype, DeviceType device)
+        : shape_(shape), dtype_(dtype), device_(device) {
+
+        int64_t expected_size = calculateSize();
+        if (data.size() != expected_size) {
+            throw std::invalid_argument("Data size doesn't match shape");
+        }
+
+        // Allocate memory and copy data
+        size_t byte_size = expected_size * sizeof(T);
+        data_ = std::shared_ptr<void>(
+            malloc(byte_size),
+            [](void* ptr) { free(ptr); }
+        );
+
+        if (device_ == DeviceType::CPU) {
+            std::memcpy(data_.get(), data.data(), byte_size);
+        }
+        else {
+            // TODO: Implement CUDA memory allocation and copy
+            throw std::runtime_error("CUDA not implemented yet");
+        }
+    }
+
+    Tensor::Tensor(const std::vector<int64_t>& shape, DType dtype, DeviceType device)
+        : shape_(shape), dtype_(dtype), device_(device) {
+
+        allocateMemory();
+
+        // Initialize to zeros
+        size_t byte_size = calculateSize() * getSizeOfDType(dtype_);
+        if (device_ == DeviceType::CPU) {
+            std::memset(data_.get(), 0, byte_size);
+        }
+        else {
+            // TODO: Implement CUDA initialization
+            throw std::runtime_error("CUDA not implemented yet");
+        }
+    }
+
+    // Copy constructor
+    Tensor::Tensor(const Tensor& other)
+        : shape_(other.shape_),
+        dtype_(other.dtype_),
+        device_(other.device_),
+        requires_grad_(other.requires_grad_) {
+
+        size_t byte_size = calculateSize() * getSizeOfDType(dtype_);
+        data_ = std::shared_ptr<void>(
+            malloc(byte_size),
+            [](void* ptr) { free(ptr); }
+        );
+
+        if (device_ == DeviceType::CPU) {
+            std::memcpy(data_.get(), other.data_.get(), byte_size);
+        }
+        else {
+            // TODO: Implement CUDA copy
+            throw std::runtime_error("CUDA not implemented yet");
+        }
+    }
+
+    // Move constructor
+    Tensor::Tensor(Tensor&& other) noexcept
+        : data_(std::move(other.data_)),
+        shape_(std::move(other.shape_)),
+        dtype_(other.dtype_),
+        device_(other.device_),
+        requires_grad_(other.requires_grad_),
+        grad_ctx_(std::move(other.grad_ctx_)) {
+
+        // Reset other to empty state
+        other.dtype_ = DType::Float32;
+        other.device_ = DeviceType::CPU;
+        other.requires_grad_ = false;
+    }
+
+    // Helper methods
+    int64_t Tensor::calculateSize() const {
+        if (shape_.empty()) return 0;
+
+        int64_t size = 1;
+        for (int64_t dim : shape_) {
+            size *= dim;
+        }
+        return size;
+    }
+
+    void Tensor::allocateMemory() {
+        size_t byte_size = calculateSize() * getSizeOfDType(dtype_);
+
+        if (device_ == DeviceType::CPU) {
+            data_ = std::shared_ptr<void>(
+                malloc(byte_size),
+                [](void* ptr) { free(ptr); }
+            );
+        }
+        else {
+            // TODO: Implement CUDA allocation
+            throw std::runtime_error("CUDA not implemented yet");
+        }
+    }
+
+    // Utility functions
+    static size_t getSizeOfDType(DType dtype) {
+        switch (dtype) {
+        case DType::Float32: return sizeof(float);
+        case DType::Float64: return sizeof(double);
+        case DType::Int32: return sizeof(int32_t);
+        case DType::Int64: return sizeof(int64_t);
+        default: throw std::runtime_error("Unknown dtype");
+        }
+    }
+
+    std::string Tensor::toString() const {
+        std::ostringstream oss;
+        oss << "Tensor(shape=[";
+        for (size_t i = 0; i < shape_.size(); ++i) {
+            if (i > 0) oss << ", ";
+            oss << shape_[i];
+        }
+        oss << "], dtype=";
+
+        switch (dtype_) {
+        case DType::Float32: oss << "float32"; break;
+        case DType::Float64: oss << "float64"; break;
+        case DType::Int32: oss << "int32"; break;
+        case DType::Int64: oss << "int64"; break;
+        }
+
+        oss << ", device=" << (device_ == DeviceType::CPU ? "cpu" : "cuda");
+        oss << ", requires_grad=" << (requires_grad_ ? "true" : "false");
+        oss << ")";
+
+        return oss.str();
+    }
+
+} // namespace dumbtorch
